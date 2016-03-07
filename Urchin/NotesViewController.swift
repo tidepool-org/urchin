tidepool-org/urchin

commit 12857a89f95bcc11dd63cb1823bcd32db8b53615

Add UI to show status of last HealthKit data sync
Trello story: https://trello.com/c/T9pCHEMi - [blip notes] [iOS] Healthkit Prototype

NOTE: The status UI is only shown after we start receiving samples. The status UI is shown beneath the "Share Health Data" row/button. The status UI shows the last sync time and count of aggregate (if within one minute) of both workout and glucose samples, or, if both types weren't sync'd within a minute of each other, then, just the count and time of last sync'd type. We may want to revisit this if we want to always show more granular status for both types.

Urchin/NotesViewController.swift
@@ -189,6 +189,10 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
         notificationCenter.addObserver(self, selector: "refreshSessionToken:", name: "refreshSessionToken", object: nil)
         // Listen for when force a logout
         notificationCenter.addObserver(self, selector: "forcedLogout:", name: "forcedLogout", object: nil)
+
+        // Handle HealthKitDataSync notifications
+        notificationCenter.addObserver(self, selector: "handleObservedHealthKitDataSyncNotification:", name: HealthKitDataSync.sharedInstance.observedBloodGlucoseSamplesNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "handleObservedHealthKitDataSyncNotification:", name: HealthKitDataSync.sharedInstance.observedWorkoutSamplesNotification, object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -512,6 +516,12 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
     }
     
+    func handleObservedHealthKitDataSyncNotification(notification: NSNotification) {
+        if (dropDownMenu != nil) {
+            dropDownMenu.reloadData()
+        }
+    }
+
     func configureDropDownMenu() {
         // Configure and add the overlay, has same height as view
         overlayHeight = self.view.frame.height
@@ -527,6 +537,9 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
         var additionalCells = groups.count == 1 ? 1 : 3
         if (HealthKitManager.sharedInstance.isHealthDataAvailable) {
             additionalCells++
+            if (HealthKitDataSync.sharedInstance.lastSyncCount > 0) {
+                additionalCells++
+            }
         }
         let proposedDropDownH = CGFloat(groups.count+additionalCells)*userCellHeight + CGFloat(groups.count)*userCellThinSeparator + 2*userCellThickSeparator
         self.dropDownHeight = min(proposedDropDownH, self.view.frame.height)
@@ -679,6 +692,9 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
                 
                 if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 0) {
                     cell.configure("healthkit", arrow: false)
+                } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 1) {
+                    cell.configure("healthkit-sync", arrow: false)
+                    cell.userInteractionEnabled = false
                 } else if (indexPath.section == sectionIndex(TableSection.Logout) && indexPath.row == 0) {
                     cell.configure("logout", arrow: false)
                 } else if (indexPath.section == sectionIndex(TableSection.Version) && indexPath.row == 0) {
@@ -690,6 +706,9 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
                     cell.configure("all")
                 } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 0) {
                     cell.configure("healthkit", arrow: false)
+                } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 1) {
+                    cell.configure("healthkit-sync", arrow: false)
+                    cell.userInteractionEnabled = false
                 } else if (indexPath.section == sectionIndex(TableSection.Logout) && indexPath.row == 0) {
                     cell.configure("logout", arrow: false)
                 } else if (indexPath.section == sectionIndex(TableSection.Version) && indexPath.row == 0) {
@@ -732,13 +751,15 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
             numberOfRows = filteredNotes.count
         } else if (tableView.isEqual(dropDownMenu)){
             if (groups.count == 1) {
-                numberOfRows = 1
+                if section == sectionIndex(TableSection.HealthKit) {
+                    numberOfRows = HealthKitDataSync.sharedInstance.lastSyncCount > 0 ? 2 : 1
+                }
             } else {
                 if (section == sectionIndex(TableSection.Users)) {
                     // Number of groups + 1 for 'All' / #nofilter
                     numberOfRows = groups.count + 1
                 } else if (section == sectionIndex(TableSection.HealthKit)) {
-                    numberOfRows = 1
+                    numberOfRows = HealthKitDataSync.sharedInstance.lastSyncCount > 0 ? 2 : 1
                 } else if (section == sectionIndex(TableSection.Logout)) {
                     numberOfRows = 1
                 } else if (section == sectionIndex(TableSection.Version)) {
@@ -818,6 +839,13 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
                     nameLabel.sizeToFit()
                     
                     return userCellInset + nameLabel.frame.height + userCellInset + (userCellThickSeparator - userCellThinSeparator)
+                } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 1) {
+                    let nameLabel = UILabel()
+                    nameLabel.text = healthKitTitle
+                    nameLabel.font = mediumBoldFont
+                    nameLabel.sizeToFit()
+                    
+                    return userCellInset + nameLabel.frame.height + userCellInset + (userCellThickSeparator - userCellThinSeparator)
                 } else if (indexPath.section == sectionIndex(TableSection.Logout) && indexPath.row == 0) {
                     let nameLabel = UILabel()
                     nameLabel.text = logoutTitle
@@ -845,6 +873,13 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
                     
                     return userCellThickSeparator + userCellInset + nameLabel.frame.height + userCellInset + userCellThinSeparator
                 } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 0) {
+                    let nameLabel = UILabel()
+                    nameLabel.text = healthKitTitle
+                    nameLabel.font = mediumBoldFont
+                    nameLabel.sizeToFit()
+                    
+                    return userCellInset + nameLabel.frame.height + userCellInset + (userCellThickSeparator - userCellThinSeparator)
+                } else if (indexPath.section == sectionIndex(TableSection.HealthKit) && indexPath.row == 1) {
                     let nameLabel = UILabel()
                     nameLabel.text = healthKitTitle
                     nameLabel.font = mediumBoldFont

Urchin/ConstantsAndStyle.swift
@@ -226,6 +226,8 @@ let allTeamsTitle: String = "All"
 Drop down menu title for HealthKit (NotesVC)
 */
 let healthKitTitle: String = "Share Health Data"
+let healthKitSyncStatusSingularFormat: String = "Shared %d sample, %@"
+let healthKitSyncStatusPluralFormat: String = "Shared %d samples, %@"
 
 /*
     Drop down menu title for logging out (NotesVC)
@@ -526,6 +528,7 @@ let userCellHeight: CGFloat = 56.0
 let userCellInset: CGFloat = labelInset
 let userCellThickSeparator: CGFloat = 1
 let userCellThinSeparator: CGFloat = 1
+let userCellHealthKitSampleInset: CGFloat = 20
 
 // ------------ NoteCell ------------
 

Urchin/HealthKit/HealthKitDataSync.swift
@@ -34,8 +34,56 @@ class HealthKitDataSync {
         
         // Set this as the configuration used for the default Realm
         Realm.Configuration.defaultConfiguration = config
+
+        var syncTime = NSUserDefaults.standardUserDefaults().objectForKey("lastSyncTimeBloodGlucoseSamples")
+        if (syncTime != nil) {
+            lastSyncTimeBloodGlucoseSamples = syncTime as! NSDate
+            lastSyncCountBloodGlucoseSamples = NSUserDefaults.standardUserDefaults().integerForKey("lastSyncCountBloodGlucoseSamples")
+        }
+        
+        syncTime = NSUserDefaults.standardUserDefaults().objectForKey("lastSyncTimeWorkoutSamples")
+        if (syncTime != nil) {
+            lastSyncTimeWorkoutSamples = syncTime as! NSDate
+            lastSyncCountWorkoutSamples = NSUserDefaults.standardUserDefaults().integerForKey("lastSyncCountWorkoutSamples")
+        }
     }
- 
+    
+    internal(set) var lastSyncCountBloodGlucoseSamples = -1
+    internal(set) var lastSyncTimeBloodGlucoseSamples = NSDate.distantPast()
+
+    internal(set) var lastSyncCountWorkoutSamples = -1
+    internal(set) var lastSyncTimeWorkoutSamples = NSDate.distantPast()
+    
+    var lastSyncCount: Int {
+        get {
+            let time = lastSyncTime
+            var count = 0
+            if (lastSyncCountBloodGlucoseSamples > 0 && fabs(lastSyncTimeBloodGlucoseSamples.timeIntervalSinceDate(time)) < 60) {
+                count += lastSyncCountBloodGlucoseSamples
+            }
+            if (lastSyncCountWorkoutSamples > 0 && fabs(lastSyncTimeWorkoutSamples.timeIntervalSinceDate(time)) < 60) {
+                count += lastSyncCountWorkoutSamples
+            }
+            return count
+        }
+    }
+    
+    var lastSyncTime: NSDate {
+        get {
+            var time = NSDate.distantPast()
+            if (lastSyncCountBloodGlucoseSamples > 0 && time.compare(lastSyncTimeBloodGlucoseSamples) == .OrderedAscending) {
+                time = lastSyncTimeBloodGlucoseSamples
+            }
+            if (lastSyncCountWorkoutSamples > 0 && time.compare(lastSyncTimeWorkoutSamples) == .OrderedAscending) {
+                time = lastSyncTimeWorkoutSamples
+            }
+            return time
+        }
+    }
+
+    let observedBloodGlucoseSamplesNotification = "HealthKitDataSync-observed-\(HKWorkoutTypeIdentifier)"
+    let observedWorkoutSamplesNotification = "HealthKitDataSync-observed-\(HKWorkoutTypeIdentifier)"
+
     func authorizeAndStartSyncing(
             shouldSyncBloodGlucoseSamples shouldSyncBloodGlucoseSamples: Bool,
             shouldSyncWorkoutSamples: Bool)
@@ -73,8 +121,10 @@ class HealthKitDataSync {
                         if (deletedSamples != nil) {
                             NSLog("********* PROCESSING \(deletedSamples!.count) deleted blood glucose samples ********* ")
                         }
-
+                        
                         self.writeSamples(newSamples: newSamples, deletedSamples: deletedSamples, error: error)
+                        
+                        self.updateLastSyncBloodGlucoseSamples(newSamples: newSamples, deletedSamples: deletedSamples)
                     }
                     HealthKitManager.sharedInstance.enableBackgroundDeliveryBloodGlucoseSamples()
                 }
@@ -91,6 +141,8 @@ class HealthKitDataSync {
                         }
 
                         self.writeSamples(newSamples: newSamples, deletedSamples: deletedSamples, error: error)
+                        
+                        self.updateLastSyncWorkoutSamples(newSamples: newSamples, deletedSamples: deletedSamples)
                     }
                     HealthKitManager.sharedInstance.enableBackgroundDeliveryWorkoutSamples()
                 }
@@ -178,6 +230,50 @@ class HealthKitDataSync {
             }
         } catch let error as NSError! {
             NSLog("\(__FUNCTION__): Error writing deleted samples \(error), \(error.userInfo)")
+        }
+    }
+    
+    @available(iOS 9, *)
+    private func updateLastSyncBloodGlucoseSamples(newSamples newSamples: [HKSample]?, deletedSamples: [HKDeletedObject]?) {
+        var totalCount = 0
+        if (newSamples != nil) {
+            totalCount += newSamples!.count
+        }
+        if (deletedSamples != nil) {
+            totalCount += deletedSamples!.count
+        }
+        if (totalCount > 0) {
+            lastSyncCountBloodGlucoseSamples = totalCount
+            lastSyncTimeBloodGlucoseSamples = NSDate()
+            NSUserDefaults.standardUserDefaults().setObject(lastSyncTimeBloodGlucoseSamples, forKey: "lastSyncTimeBloodGlucoseSamples")
+            NSUserDefaults.standardUserDefaults().setInteger(lastSyncCountBloodGlucoseSamples, forKey: "lastSyncCountBloodGlucoseSamples")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: self.observedBloodGlucoseSamplesNotification, object: nil))
+            }
+        }
+    }
+    
+    @available(iOS 9, *)
+    private func updateLastSyncWorkoutSamples(newSamples newSamples: [HKSample]?, deletedSamples: [HKDeletedObject]?) {
+        var totalCount = 0
+        if (newSamples != nil) {
+            totalCount += newSamples!.count
+        }
+        if (deletedSamples != nil) {
+            totalCount += deletedSamples!.count
+        }
+        if (totalCount > 0) {
+            lastSyncCountWorkoutSamples = totalCount
+            lastSyncTimeWorkoutSamples = NSDate()
+            NSUserDefaults.standardUserDefaults().setObject(lastSyncTimeWorkoutSamples, forKey: "lastSyncTimeWorkoutSamples")
+            NSUserDefaults.standardUserDefaults().setInteger(lastSyncCountWorkoutSamples, forKey: "lastSyncCountWorkoutSamples")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: self.observedWorkoutSamplesNotification, object: nil))
+            }
         }
     }
 }

Urchin/UserDropDownCell.swift
@@ -51,25 +51,40 @@ class UserDropDownCell: UITableViewCell {
             separator.backgroundColor = whiteQuarterAlpha
             self.addSubview(separator)
         } else if (key == "healthkit") {
-            self.nameLabel.text = healthKitTitle
+            nameLabel.text = healthKitTitle
             nameLabel.font = mediumBoldFont
             nameLabel.sizeToFit()
             nameLabel.frame.origin.y = userCellThickSeparator + userCellInset
             
-            // Configure the thick separator at the top
-            // take out the height of the thin separator because the cell above has a thin separator at the bottom
-            separator.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: userCellThickSeparator)
+            // Configure the separator at the top
+            separator.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: userCellThinSeparator)
+            separator.backgroundColor = whiteQuarterAlpha
+            self.addSubview(separator)
+        } else if (key == "healthkit-sync") {
+            let lastSyncCount = HealthKitDataSync.sharedInstance.lastSyncCount
+            let lastSyncTime = NSDateFormatter.localizedStringFromDate(HealthKitDataSync.sharedInstance.lastSyncTime, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
+            if (lastSyncCount == 1) {
+                nameLabel.text = String(format: healthKitSyncStatusSingularFormat, HealthKitDataSync.sharedInstance.lastSyncCount, lastSyncTime)
+                
+            } else if (lastSyncCount > 1) {
+                nameLabel.text = String(format: healthKitSyncStatusPluralFormat, HealthKitDataSync.sharedInstance.lastSyncCount, lastSyncTime)
+            }
+            nameLabel.font = smallRegularFont
+            nameLabel.sizeToFit()
+            nameLabel.frame.origin = CGPoint(x: 3 * userCellInset, y: userCellThinSeparator + userCellHealthKitSampleInset)
+            
+            // Configure the separator
+            separator.frame = CGRect(x: 2 * userCellInset, y: 0, width: self.frame.width - 2 * userCellInset, height: userCellThinSeparator)
             separator.backgroundColor = whiteQuarterAlpha
             self.addSubview(separator)
         } else if (key == "logout") {
-            self.nameLabel.text = logoutTitle
+            nameLabel.text = logoutTitle
             nameLabel.font = mediumBoldFont
             nameLabel.sizeToFit()
             nameLabel.frame.origin.y = userCellThickSeparator + userCellInset
             
-            // Configure the thick separator at the top
-            // take out the height of the thin separator because the cell above has a thin separator at the bottom
-            separator.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: userCellThickSeparator)
+            // Configure the thin separator at the top
+            separator.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: userCellThinSeparator)
             separator.backgroundColor = whiteQuarterAlpha
             self.addSubview(separator)
         } else if (key == "group") {
@@ -87,7 +102,7 @@ class UserDropDownCell: UITableViewCell {
             self.nameLabel.frame.origin = CGPoint(x: 3*userCellInset, y: userCellInset)            
         } else if (key == "version") {
             // Configure the name label to contain the version
-            self.nameLabel.text = UIApplication.versionBuildServer()
+            nameLabel.text = UIApplication.versionBuildServer()
             nameLabel.font = smallRegularFont
             nameLabel.sizeToFit()
             nameLabel.frame.origin.x = self.frame.width / 2 - nameLabel.frame.width / 2
@@ -115,7 +130,7 @@ class UserDropDownCell: UITableViewCell {
         self.group = group
         
         // configure the name label with the group name
-        self.nameLabel.text = group.fullName
+        nameLabel.text = group.fullName
         if (bold) {
             nameLabel.font = mediumBoldFont
         } else {

commit d9569aca94bd84f4e9bb328727f37be7377ae6cd

Fix bug https://trello.com/c/k9s5xryj
Fix bug caused by regression in commit 12857a89f95bcc11dd63cb1823bcd32db8b53615. The default numberOfRows for sections is 0. The numberOfRows for sections for this cell for an account with one group used to be 1, but, the aforementioned commit removed that, except for the HealthKit section, thus resulting in now content showing for other sections, notably Logout and Version.

Urchin/NotesViewController.swift
@@ -752,6 +752,7 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
             numberOfRows = filteredNotes.count
         } else if (tableView.isEqual(dropDownMenu)){
             if (groups.count == 1) {
+                numberOfRows = 1
                 if section == sectionIndex(TableSection.HealthKit) {
                     numberOfRows = HealthKitDataCache.sharedInstance.lastCacheCount > 0 ? 2 : 1
                 }